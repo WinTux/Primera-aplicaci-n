d169f3ab9287ca38c7c745457160a760f07598b1
rusokverse <rusokverse@MACBOOKPRO>
2022-08-23 19:50:24 -0400

    Agregar archivos de proyecto.

---
+++ Primera aplicación/Persona.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pais
+{
+    public class Persona
+    {
+        public int edad { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+
+        public Persona() {
+            edad = 18;
+            nombre = "NA";
+        }
+        public Persona(int edad, string nombre) {
+            this.edad = edad;
+            this.nombre = nombre;
+        }
+        public Persona(int edad, string nombre, string apellido)
+        {
+            this.edad = edad;
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+    }
+    public class Animal { 
+        public int edad { get; set; }
+        public int patas { get; set; }
+        public double peso { get; set; }
+    }
+
+    public class Estudiante : Persona { 
+        public int matricula { get; set; }
+
+        public Estudiante() {
+            matricula = 0;
+        }
+
+        public Estudiante(int edadz, string nombrez, string apellidoz, int matricula) : base(edadz,nombrez, apellidoz){
+            this.matricula = matricula;
+        }
+    }
+}
+
+namespace mascotas {
+    using Pais;
+    public class Perro : Animal { 
+        public string nombre { get; set; }
+        public string nombreDueño { get; set; }
+
+    }
+
+    namespace pequeños {
+        public class Loro : Animal { 
+            public string color { get; set; }
+        }
+    }
+}
---
+++ Primera aplicación/Program.cs
@@ -0,0 +1,62 @@
+﻿using mascotas;
+using Pais;
+using System;
+
+namespace ejemplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 4;
+            string texto = "Pepe";
+            bool interruptor = true;
+            Console.WriteLine(4.56 - 7.0 * 2.0 / 3.0 + 2.678f);//float - double
+            float num1 = 2.8f;
+            int b = (int)2.5;
+            Console.WriteLine(b);
+            float otroNum = b;
+            Saludo();
+            Persona per = new Persona();
+            per.edad = 20;
+            per.nombre = "Pepe";
+            Persona pp = null;
+            pp = new Persona();
+            //pp.nombre = "ana";
+
+            Persona p = new Persona(23, "Sofía", "Loza");
+            Persona z = new Persona(20, "Rocha","Sofía");
+
+            Perro snoopy = new Perro();
+            snoopy.patas = 4;
+            snoopy.nombre = "Batuque";
+            mascotas.pequeños.Loro lorito = new mascotas.pequeños.Loro();
+            
+            Console.WriteLine(per.edad + "; " + per.nombre);
+            Console.WriteLine(pp.nombre);
+
+            //constructores en clases heredadas
+
+            Estudiante est1 = new Estudiante();
+
+            Estudiante est2 = new Estudiante(19, "Arturo", "Roca", 401);
+
+            Persona ppp = est2;
+            //Estudiante eee = (Estudiante)p;
+
+            Console.WriteLine(est2.matricula + "-" + est2.nombre);
+            
+            Console.WriteLine( ppp.apellido + "-" + ppp.nombre);
+
+            Estudiante est2aux = (Estudiante)ppp;
+            Console.WriteLine(est2aux.matricula + "-" + est2aux.nombre);
+
+            Console.ReadKey();
+        }
+
+        private static void Saludo() {
+            Console.WriteLine("Hola");
+        }
+    }
+}
